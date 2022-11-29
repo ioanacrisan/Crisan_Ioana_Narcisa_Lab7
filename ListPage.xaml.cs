392c9a7450729ccabe720566665cab692361659c
IOANA-NARCISA CRIȘAN <crisanioana@protonmail.com>
2022-11-29 14:54:19 +0200

    Laborator 8

---
+++ ListPage.xaml.cs
@@ -0,0 +1,25 @@
+using Crisan_Ioana_Narcisa_Lab7.Models;
+
+namespace Crisan_Ioana_Narcisa_Lab7;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+}
